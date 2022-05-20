c9814caa814de54877f292fee8cc1f302123a83b
Arwalk <SirAgentProvocateurArwalk@gmail.com>
2022-05-20 16:23:51 +0200

    building up our first ines rom

--- src/lib/ines.zig
+++ src/lib/ines.zig
@@ -6,59 +6,48 @@ const iNesROMVersion = enum {
     V2
 };
 
-const TvSystem = enum(u1) {
-    NTSC = 0,
-    PAL = 1
+const MirroringType = enum(u1) {
+    HORIZONTAL_OR_MAPPER= 0,
+    VERTICAL = 1
 };
 
 pub const INesROM = struct {
 
     const InvalidFileError = error {
-        NotINes
+        NotINes,
+        NotManaged, // for V2 ines format, for now
     };
 
     const INesFlags = struct {
         // f6
-        mirroring: bool,                // b0 horizontal (vertical arrangement) (CIRAM A10 = PPU A11) | vertical (horizontal arrangement) (CIRAM A10 = PPU A10)
-        battery_backed_prg_ram: bool,   // b1 Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory
-        is_trainer_present: bool,       // b2 512-byte trainer at $7000-$71FF (stored before PRG data)
-        ignore_mirroring_control: bool, // b3 Ignore mirroring control or above mirroring bit; instead provide four-screen VRAM
-        // f9
-        tv_system: TvSystem,
-        // f10
-        has_bus_conflicts: bool,
+        mirroring: MirroringType,               // b0 horizontal (vertical arrangement) (CIRAM A10 = PPU A11) | vertical (horizontal arrangement) (CIRAM A10 = PPU A10)
+        battery_backed_prg_ram: bool,           // b1 Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory
+        is_trainer_present: bool,               // b2 512-byte trainer at $7000-$71FF (stored before PRG data)
+        hard_wired_four_screen_mode: bool,      // b3 Ignore mirroring control or above mirroring bit; instead provide four-screen VRAM
     };
 
-    const PgrData = struct {
-        size: u8,
-        ram_size: u8,
-        program_rom: [16384 * 255]u8
-    };
+    const prgrdata_multiplier = 16384;
+    const chrdata_multiplier = 8192;
 
-    const ChrData = struct {
-        size: u8,
-        rom: [8192 * 255]u8
-    };
-
-    pgr: PgrData,
-    chr: ChrData,
+    pgr_rom: []u8,
+    chr_rom: []u8,
     mapper: u8,
     flags: INesFlags,
-    trainer: ?[512]u8,
+    trainer: ?[]u8,
 
-    pub fn from_file_path(file_path: []const u8) !void {
+    pub fn from_file_path(file_path: []const u8, allocator: *std.mem.Allocator) !INesROM {
         debug("from_file_path : opening file {s}", .{file_path});
         
         var file_handle = try std.fs.openFileAbsolute(file_path, .{.read = true, .write = false});
         defer file_handle.close();
 
-        var buffer = [_]u8{0} ** (16 + 512 + (16384 * 255) + (8192 * 256) + 32);
-        const size_read = file_handle.readAll(&buffer);
+        var buffer = try file_handle.readToEndAlloc(allocator);
+        defer allocator.free(buffer);
 
-        try from_buffer(&buffer);
+        return try from_buffer(&buffer, allocator);
     }
 
-    pub fn from_buffer(buffer: []const u8) !void {
+    pub fn from_buffer(buffer: []const u8, allocator: *std.mem.Allocator) !INesROM {
         if(
             buffer[0] != 0x4E or
             buffer[1] != 0x45 or
@@ -69,6 +58,59 @@ pub const INesROM = struct {
         debug("file is INes!", .{});
 
 
+        // parsing header
+        const header = buffer[0..16];
+        if((header[7]&0x0C)==0x08)
+        {
+            return InvalidFileError.NotManaged;
+        }
+
+        const pgr_size : usize = header[4] + @intCast(u12, header[9] & 0xF);
+        const chr_size : usize = header[5] + @intCast(u12, header[9] >> 4);
+        debug("pgr_size = {}", .{pgr_size});
+        debug("chr_size = {}", .{chr_size});
+
+        const flags : INesFlags = .{
+            .mirroring = @intToEnum(MirroringType, @intCast(u1, header[6] & 1)),
+            .battery_backed_prg_ram = ((header[6] & (1 << 1)) != 0),
+            .is_trainer_present = ((header[6] & (1 << 2)) != 0),
+            .hard_wired_four_screen_mode = ((header[6] & (1 << 3)) != 0),
+        };
+
+        const mapper: u8 = ((header[6] & 0xF0) >> 4) + (header[7] & 0xF0);
+
+        const pgr_rom = try allocator.alloc(u8, pgr_size * prgrdata_multiplier);
+        errdefer {
+            allocator.free(pgr_rom);
+        }
+
+        const chr_rom = try allocator.alloc(u8, chr_size * chrdata_multiplier);
+        errdefer {
+            allocator.free(chr_rom);
+        }
+
+        var trainer : ?[]u8 = undefined;
+        if(flags.is_trainer_present){
+            trainer = try allocator.alloc(u8, 512);
+        }
+        else
+        {
+            trainer = null;
+        }
+
+        const rom = INesROM{
+            .pgr_rom = pgr_rom,
+            .chr_rom = chr_rom,
+            .mapper = mapper,
+            .flags = flags,
+            .trainer = trainer
+        };
+        return rom;
+    }
+
+    pub fn free(self: *INesROM, allocator: *std.mem.Allocator) void {
+        allocator.free(self.pgr_rom);
+        allocator.free(self.chr_rom);
     }
 
 };
--- tests/tests_cpu_roms.zig
+++ tests/tests_cpu_roms.zig
@@ -1,11 +1,13 @@
-const ines = @import("ines.zig");
-const cpu = @import("cpu.zig");
+const ines = @import("ines");
+const cpu = @import("cpu");
+const std = @import("std");
+const test_allocator = std.testing.allocator;
 
 const all_instrs = @embedFile("test_files/all_instrs.nes");
 
 test "official only" {
     const official_only = @embedFile("test_files/official_only.nes");
 
-    
-
+    var rom = try ines.INesROM.from_buffer(official_only[0..official_only.len], test_allocator);
+    defer rom.free(test_allocator);
 }
